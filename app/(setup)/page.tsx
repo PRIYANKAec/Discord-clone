3ce0a59aaa7a8b4ae8f4ad3c28d15c9da8212264
PRIYANKAec <priyankaa261103@gmail.com>
2024-05-22 18:43:01 +0530

    partially finished

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,9 +1,23 @@
+import {db} from "@/lib/db";
+import  {initialProfile} from "@/lib/initial-profile";
+import {redirect} from "next/navigation";
+
 const SetupPage= async () => {
-    return ( 
-        <div>
-            Create a server
-        </div>
-     );
+    const profile = await initialProfile();
+    const server = await db.server.findFirst ({
+        where:{
+            members:{
+                some:{
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+    if(server){
+        return redirect(`/servers/${server.id}`);
+    }
+        return <div>Create a server </div>;
+    
 }
  
 export default SetupPage;
