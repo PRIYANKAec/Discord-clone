b838e414689c69f7fc810f02789099a496e80ef8
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-02 19:45:43 +0530

    Server_Settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal"
+import { EditServerModal } from "@/components/modals/edit-server-modal"
 import { InviteModal } from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
@@ -20,6 +21,7 @@ export const ModalProvider = () => {
         <>
         <CreateServerModal/>
         <InviteModal/>
+        <EditServerModal />
         </>
     )
 }
