d2ae7c1ce55221ca6054d6c88d39ceca6222cc56
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-07 10:41:29 +0530

    Channel ID Page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -26,8 +26,13 @@ export const ServerMember = ({
 
     const icon = roleIconMap[member.role];
 
+    const onClick = () => {
+        router.push(`/servers/${server.id}/conversations/${member.id}`);
+    }
+
     return (
         <button
+        onClick={onClick}
         className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
