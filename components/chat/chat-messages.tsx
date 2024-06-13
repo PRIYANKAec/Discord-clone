608fbd293ea35816279a8aba3024ba704e8ce2e8
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-13 14:38:37 +0530

    real time chat page update[ADD]

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,7 @@ import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 import { ChatWelcome } from "./chat-welcome";
 import { ChatItem } from "./chat-item";
@@ -41,6 +42,8 @@ export const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`;
 
     const {
         data,
@@ -54,6 +57,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue,
       });
+      useChatSocket({ queryKey, addKey, updateKey});
 
       if (status === "pending") {
         return (
