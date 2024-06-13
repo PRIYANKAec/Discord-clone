def5c803def0c35a49e877a9ff2a3b40857deb5b
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-13 21:50:05 +0530

    Implemented Audio and Video calls using LiveKit[FIX]

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import {LiveKitRoom, VideoConference} from "@livekit/components-react";
-import "@LiveKit/components-styles";
+import "@livekit/components-styles";
 import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react"; 
