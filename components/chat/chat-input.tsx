553acc34976ea91acfa2317674eaa7a9c15a9d63
PRIYANKAec <priyankaa261103@gmail.com>
2024-06-13 21:54:59 +0530

    rendered Emoji picker and emoji[FIX]

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,68 +1,68 @@
-"use client"
+"use client";
 
 import * as z from "zod";
 import axios from "axios";
 import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Plus} from "lucide-react";
-import {useRouter} from "next/navigation";
+import { Plus, Smile } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 import {
     Form,
     FormControl,
     FormField,
     FormItem,
-} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import { useModal } from "@/hooks/use-modal-store";
+  } from "@/components/ui/form";
+  import { Input } from "@/components/ui/input";
+  import { useModal } from "@/hooks/use-modal-store";
 import { EmojiPicker } from "@/components/emoji-picker";
-
 
 interface ChatInputProps {
     apiUrl: string;
     query: Record<string, any>;
     name: string;
     type: "conversation" | "channel";
-}
-
-const formSchema = z.object({
+  }
+  
+  const formSchema = z.object({
     content: z.string().min(1),
-});
-
-export const ChatInput = ({
+  });
+  
+  export const ChatInput = ({
     apiUrl,
     query,
     name,
     type,
-}: ChatInputProps) => {
-    const {onOpen} = useModal();
-
+  }: ChatInputProps) => {
+    const { onOpen } = useModal();
+    const router = useRouter();
+  
     const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            content: "",
-        }
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        content: "",
+      }
     });
-
+  
     const isLoading = form.formState.isSubmitting;
-
+  
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            const url = qs.stringifyUrl({
-              url: apiUrl,
-              query,
-            });
-      
-            await axios.post(url, values);
-      
-            form.reset();
-            router.refresh();
-          } catch (error) {
-            console.log(error);
-          }
-    }
-
+          const url = qs.stringifyUrl({
+            url: apiUrl,
+            query,
+          });
+    
+          await axios.post(url, values);
+    
+          form.reset();
+          router.refresh();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+  
     return (
         <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -98,5 +98,5 @@ export const ChatInput = ({
         />
       </form>
     </Form>
-    )
+  )
 }
